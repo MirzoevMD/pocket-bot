c5c09c4ec59c1a2685f25f7e0ba757d06eb1e869
MirzoevMD <saidoislomiddinovich@gmail.com>
2025-07-12 20:58:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import time
+
+def main():
+    print("Бот запущен и работает...")
+    while True:
+        time.sleep(10)
+        print("Анализ данных...")
+
+if __name__ == "__main__":
+    main()
